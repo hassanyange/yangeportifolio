8f6a7ce56be9bf62d6c0f8423fc2b0b0c531ec89
hassanyange <hassanyange@gmail.com>
2022-04-19 18:28:21 +0300

    models issues

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Cards(models.Model):
     imageupload = models.ImageField(upload_to = 'media')
-    Title_of_card = models.CharField(max_length= 30)
+    Title_of_card = models.CharField(max_length= 30 ,default= 'Title of card')
     heroku_link = models.CharField(max_length=30)
     github_link = models.CharField(max_length=30)
     commment = models.CharField(max_length=100)
